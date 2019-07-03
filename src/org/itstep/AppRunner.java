31d552ae41ceeb0ba34cd9f7d7e195e6af7bcacf
JuliaShilo <akira677@mail.ru>
2019-07-03 20:53:28 +0300

    Create Randomizer

--- src/org/itstep/AppRunner.java
+++ src/org/itstep/AppRunner.java
@@ -1,6 +1,26 @@
 package org.itstep;
 
 
-public class AppRunner {
+import org.itstep.model.ConnectionData;
+import org.itstep.service.FileManagerService;
+import org.itstep.util.Randomizer;
 
+import java.util.Date;
+
+public class AppRunner {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            ConnectionData connectionData = new ConnectionData();
+            connectionData.setSessionId(Randomizer.getRandomNumber(10_000_000, 99_999_999));
+            connectionData.setUserLogin(Randomizer.gerRandomString(10));
+            connectionData.setTime(new Date() .getTime() - Randomizer.getRandomNumber(0, 1000 * 60 * 60 * 24)) ;
+            connectionData.setIp(Randomizer.getRandomNumber(100,225) + "." +
+                    Randomizer.getRandomNumber(100,225) + "." +
+                    Randomizer.getRandomNumber(100,225) + "." +
+                    Randomizer.getRandomNumber(100,225));
+            FileManagerService.writeDatatoFile(connectionData, true);
+
+        }
+    }
 }
--- src/org/itstep/model/ConnectionData.java
+++ src/org/itstep/model/ConnectionData.java
@@ -1,18 +1,41 @@
 package org.itstep.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ConnectionData {
 
-    private Integer id;
+    private Integer sessionId;
     private String ip;
     private Long time;
+    private String userLogin;
 
+    public Integer getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Integer sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
 }
